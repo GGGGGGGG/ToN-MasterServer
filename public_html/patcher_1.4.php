eb4f936ed26897bb1d8f86338c2cef483b771e85
Mohammed A <mohammed@d6166.cs.queensu.ca>
2014-11-16 20:46:10 -0500

    Minor fix

--- public_html/patcher_1.4.php
+++ public_html/patcher_1.4.php
@@ -7,30 +7,40 @@ $clientver = post_input("version");
 if($clientver == "") return;
 $clientos = post_input("os");
 $clientarch = post_input("arch");
+patchesdb_open();
 $query = "select concat(major, '.', minor, '.', build, '.', revision) as ver
 	from patches.version
 	order by date limit 1";
 	
 $result = db_query($query);
 
-if(!mysql_num_rows($result) == 1) {
+if(mysqli_num_rows($result) != 1) {
 	/* Something's terribly wrong */
 	return;	
 } else {
 	/* Return user data */
-	$data = mysql_fetch_assoc($result);
+	$data = mysqli_fetch_assoc($result);
 	$curver = $data["ver"];
-	if($clientver < $curver) {
+	if($clientver == $curver) {
+		echo serialize(array("version" => $curver));
+	}
+	else if($clientver < $curver) {
 		/* inform client of new patches */
-		$query = "select name, concat(major, minor, build, revision) as version, os, arch from patches.version_check 
-			where version=$curver 
+		$query = "select name, concat(major, '.', minor, '.', build, '.', revision) as version, os, arch from patches.version_check 
+			where concat(major, '.', minor, '.', build, '.', revision)=\"$curver\" 
 			ORDER BY name";
 		$result = db_query($query);
-		$nRows = mysql_num_rows($result);
+		$nRows = mysqli_num_rows($result);
 		$res = array();
 		for($i=0; $i < $nRows; ++$i) {
-			array_push($res, array("name" => name, "version" => version, "os" => os, "arch" => arch));
+			$data = mysqli_fetch_assoc($result);
+			$name = $data["name"];
+			$version = $data["version"];
+			$os = $data["os"];
+			$arch = $data["arch"];
+			array_push($res, array("name" => $name, "version" => $version, "os" => $os, "arch" => $arch));
 		}
+		$res["version"] = $curver;
 		echo serialize($res);
 	}
 }
