5ec58a463f6c2b7ade450285e83d709e0ea9ba5d
Cedeqien <cedekien@gmail.com>
2018-01-20 12:26:08 -0500

    cronjob added for lifetime sf

---
+++ cron/sf.php
@@ -0,0 +1,26 @@
+<?php
+
+include("../common/lib.php");
+
+function calculate_sf()
+{
+    global $dbcon;
+    $players = array();
+    $query = "SELECT id FROM users WHERE updated_at >= (CURDATE() - INTERVAL 30 DAY)";
+    $result = mysqli_query($dbcon, $query);
+
+    while($row = mysqli_fetch_array($result))
+    {
+        $players = $row;
+    }
+
+    foreach($players as $player)
+    {
+        $query = "SELECT AVG(sf) as sf_avg from actionplayers where user = {$player}";
+        $result = mysqli_query($dbcon, $query);
+        $sf = mysqli_fetch_assoc($result);
+
+        $query = "UPDATE playerstats SET sf='{$sf['sf_avg']}' WHERE id='{$player}'";
+        mysqli_query($dbcon, $query);
+    }
+}
